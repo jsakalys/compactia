b8ce099980e855d3bf3433129b265127752d0854
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2015-10-31 23:08:10 -0700

    Performed some CSS tweaks

--- controllers/characters.js
+++ controllers/characters.js
@@ -8,7 +8,7 @@ var db = require("./../models");
 /* Routers */
 
 // Shows a list of characters belonging to the current user
-router.get('/', function(req,res){
+router.get('/list', function(req,res){
 	res.render('characters/list', { layout: 'layouts/account-view' })
 });
 
@@ -19,18 +19,15 @@ router.get('/new', function(req,res){
 
 // Catches new character form data
 router.post('/new', function(req,res){
-	console.log(req.body);
-	res.send('created');
 	//remember to search for user, and then associate that user's new character to user
 
-	// db.character.create({
-	//     name: req.body.name,
-	//     identifier: req.body.identifier,
-	//     password: req.body.password,
-	//     type: req.body.type
-	// }).then(function() {
-	//     res.redirect('list');
-	// });
+	db.character.create({
+	    name: req.body.name,
+	    exp: parseInt(req.body.exp),
+	    gold: parseInt(req.body.gold)
+	}).then(function() {
+	    res.redirect('list');
+	});
 });
 
 // Shows information about a given character with an option to edit it
